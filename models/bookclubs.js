a5f22f92b177810712bc09e800ef86e49583d45e
Audrey Seignol <aseignol@yahoo.fr>
2025-05-27 20:31:43 +0200

    update bookclub model

--- models/bookclubs.js
+++ models/bookclubs.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose');
 const bookclubSchema = mongoose.Schema({
     name: String,
     private: Boolean,
+    description: String,
+    icon: String,
+    creator: { type: mongoose.Schema.Types.ObjectId, ref: 'users' },
+    admins: [{ type: mongoose.Schema.Types.ObjectId, ref: 'users' }],
 });
 
 const Bookclub = mongoose.model('bookclubs', bookclubSchema);
