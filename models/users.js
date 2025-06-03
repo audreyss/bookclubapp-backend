2177c5550f72003a7cbc6c73fd2e1e496d08282b
Audrey Seignol <aseignol@yahoo.fr>
2025-06-03 19:40:57 +0200

    add icon to user model

--- models/users.js
+++ models/users.js
@@ -4,6 +4,10 @@ const userSchema = mongoose.Schema({
     pseudo: String,
     email: String,
     password: String,
+    icon: {
+        type: String,
+        default: 'https://res.cloudinary.com/dp4ymsmb5/image/upload/v1748961695/ylulsj0ndtymz7ksi4in.png'
+    }
 });
 const User = mongoose.model('users', userSchema);
 
